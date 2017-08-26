c24a81dc22df0bd59d409097a15671e00297c700
ajaymann <ajaymann7@gmail.com>
2017-08-26 12:25:53 +0530

    Initial Commit

---
+++ Giphsy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Giphsy
+//
+//  Created by Ajay Mann on 8/26/17.
+//  Copyright © 2017 Ajay Mann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
